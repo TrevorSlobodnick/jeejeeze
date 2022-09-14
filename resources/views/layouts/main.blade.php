ca2f23ce2e2e8dc2fd979781e402192e384a3fe3
TrevorSlobodnick <trevorslobodnick@gmail.com>
2022-09-14 19:10:41 -0400

    Resolved issue with blade-icons, added logo font/svg, and made minor changes to the nav

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,40 +9,50 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-dark p-0 navbar-expand-lg">
-            <span class="navbar-brand mb-0 h1">JeeJeeze</span>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-container" aria-controls="nav-container" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="nav-container">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href={{ URL::to('/store-locator') }}>
-                            @svg('home')
-{{--                            <x-icon name="home.svg" />--}}
-                            Stores
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href={{ URL::to('/account') }}>
-{{--                            <x-icon name="user" />--}}
-                            Account
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href={{ URL::to('/cart') }}>
-{{--                            <x-icon name="shopping-cart" />--}}
-                            Cart
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div class="max-width">
+            <nav class="navbar navbar-dark p-0 navbar-expand-md">
+                <a href="{{ URL::to("/")  }}" class="navbar-brand m-0">
+                    <x-icon name="gg" />
+                    <span class="brand">JeeJeeze</span>
+                </a>
+                <button class="navbar-toggler hide-border-boxshadow" type="button" data-bs-toggle="offcanvas" data-bs-target="#nav-container" aria-controls="nav-container" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end" id="nav-container">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                        <button type="button" class="btn-close btn-close-white hide-border-boxshadow" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href={{ URL::to('/store-locator') }}>
+                                    <x-icon name="home" />
+                                    <span>Stores</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href={{ URL::to('/account') }}>
+                                    <x-icon name="user" />
+                                    <span>Account</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href={{ URL::to('/cart') }}>
+                                    <x-icon name="shopping-cart" />
+                                    <span>Cart</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </header>
-    <div class="container-fluid">
+    <div class="max-width">
         @yield('content')
     </div>
-    <footer class="text-center py-3">
+    <footer class="text-center py-3 px-2">
         &copy; 2022 Trevor Slobodnick | All Rights Reserved
     </footer>
 </body>
