5e1c05c68031263b7458e94ff959fbdcf02b45f1
TrevorSlobodnick <trevorslobodnick@gmail.com>
2022-09-14 03:13:08 -0400

    WIP - Implementing Blade Icons
    I also added some minor changes to the css and html, changing it to what the actual website might look like.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,22 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JeeJeeze</title>
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <span class="navbar-brand mb-0 h1">Navbar</span>
+        <nav class="navbar navbar-dark p-0 navbar-expand-lg">
+            <span class="navbar-brand mb-0 h1">JeeJeeze</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-container" aria-controls="nav-container" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="nav-container">
-                <ul class="navbar-nav me-auto">
+            <div class="collapse navbar-collapse justify-content-end" id="nav-container">
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="{{ Route::is('home') ? 'nav-link active' : 'nav-link' }}" href={{ URL::to('/') }}>Home</a>
+                        <a class="nav-link active" href={{ URL::to('/store-locator') }}>
+                            @svg('home')
+{{--                            <x-icon name="home.svg" />--}}
+                            Stores
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a class="{{ Route::is('test') ? 'nav-link active' : 'nav-link' }}" href={{ URL::to('/test') }}>Test</a>
+                        <a class="nav-link active" href={{ URL::to('/account') }}>
+{{--                            <x-icon name="user" />--}}
+                            Account
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href={{ URL::to('/cart') }}>
+{{--                            <x-icon name="shopping-cart" />--}}
+                            Cart
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -29,7 +42,7 @@
     <div class="container-fluid">
         @yield('content')
     </div>
-    <footer>
+    <footer class="text-center py-3">
         &copy; 2022 Trevor Slobodnick | All Rights Reserved
     </footer>
 </body>
